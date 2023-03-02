ce2920d50b3923f9b6b7d2f00fc371f1675b9095
aautoruns <galihazella06@gmail.com>
2023-03-03 00:01:16 +0700

    tailwindcss portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['index.html'],
+  content: ['index.html', './node_modules/flowbite/**/*.js'],
   theme: {
     container: {
       center: true,
