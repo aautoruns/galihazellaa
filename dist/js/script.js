fa36259573c99dce60f1cc510e98996044508829
aautoruns <galihazella06@gmail.com>
2023-03-05 15:27:42 +0700

    add portfolio sweetology

--- dist/js/script.js
+++ dist/js/script.js
@@ -124,3 +124,22 @@ themeToggleBtn.addEventListener('click', function () {
     }
   }
 });
+
+// FAQ
+const accordionHeader = document.querySelectorAll('.accordion-header');
+accordionHeader.forEach((header) => {
+  header.addEventListener('click', function () {
+    const accordionContent = header.parentElement.querySelector('.accordion-content');
+    let accordionMaxHeight = accordionContent.style.maxHeight;
+
+    if (accordionMaxHeight == '0px' || accordionMaxHeight.length == 0) {
+      accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32}px`;
+      header.querySelector('.fas').classList.remove('fa-plus');
+      header.querySelector('.fas').classList.add('fa-minus');
+    } else {
+      accordionContent.style.maxHeight = `0px`;
+      header.querySelector('.fas').classList.add('fa-plus');
+      header.querySelector('.fas').classList.remove('fa-minus');
+    }
+  });
+});
